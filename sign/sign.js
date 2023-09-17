10f6b5f71514127e3ab9fe7e63cee595eb0fc47a
thewakar <thewakar1@gmail.com>
2023-09-17 16:31:23 +0530

    Update and rename hi.js to sign.js

--- sign/sign.js
+++ sign/sign.js
@@ -1,4 +1,4 @@
-var signaturePad = new SignaturePad(document.getElementById('canvas'), {
+var signaturePad = new SignaturePad(document.getElementById('signature-pad'), {
   backgroundColor: 'rgba(255, 255, 255, 0)',
   penColor: 'rgb(0, 0, 0)'
 });
