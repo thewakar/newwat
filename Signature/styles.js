5f0673a4397b4be95aac954a43ce92122a9d94bd
thewakar <thewakar1@gmail.com>
2023-09-19 07:34:47 +0530

    Update styles.js

--- Signature/styles.js
+++ Signature/styles.js
@@ -4,15 +4,27 @@ document.addEventListener('DOMContentLoaded', () => {
     penColor: 'rgb(0, 0, 0)'
   });
 
-var saveButton = document.getElementById('save');
+// Save the signature to Google Drive
+async function saveSignatureToGoogleDrive() {
+  // Get the signature image data
+  const signatureImageData = signaturePad.toDataURL('image/png');
+
+  // Create a new Blob object from the signature image data
+  const signatureImageBlob = new Blob([signatureImageData]);
+
+
+  const signaturesFolder = DriveApp.createFolder('1wCuq1FCTx5hX7VfDpOUQ2Veo0GR8iXFf');
+  
+  const signatureFile = signaturesFolder.createFile(signatureImageBlob, 'signature.png');
+
+  // Save the file
+  signatureFile.save();
+}
+
+// Bind the save button to the saveSignatureToGoogleDrive function
+document.getElementById('save').addEventListener('click', saveSignatureToGoogleDrive);
+
 var cancelButton = document.getElementById('clear');
-
-saveButton.addEventListener('click', function (event) {
-  var data = signaturePad.toDataURL('image/png');
-
-// Send data to server instead...
-  window.open(data);
-});
 
 cancelButton.addEventListener('click', function (event) {
   signaturePad.clear();
